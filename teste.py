0c090ecc877c3c264845e2ae0e9c70df1ee457e0
Eduardo <eduardogo@github.com>
2020-01-03 16:25:31 -0300

    MLP notebook

--- teste.py
+++ teste.py
@@ -1,9 +1,8 @@
 import numpy
 import pandas
-import seaborn as sns
-import matplotlib.pyplot as plt
-from keras.models import Sequential
-from keras.layers import Dense
+import tensorflow
+from keras.models import Model
+from keras.layers import Dense,Input
 from keras.wrappers.scikit_learn import KerasClassifier
 from keras.utils import np_utils
 from sklearn.model_selection import cross_val_score
@@ -11,19 +10,46 @@ from sklearn.model_selection import KFold
 from sklearn.preprocessing import LabelEncoder
 import random
 from random import randrange
+import seaborn as sns
+import matplotlib.pyplot as plt
+sns.set(style="ticks")
+
+def baseline_model_MLP():
+    
+    inputs = Input(shape=(4,))
+    x = Dense(8, activation = 'relu')(inputs)
+    outputs = Dense(3, activation='softmax')(x)
+    
+    #criando o modelo
+    model = Model(input=inputs,output=outputs)
+    
+    # compilar modelo
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+    
+    return model
 
 
 
 
-seed = 7
-numpy.random.seed(seed)
+df = sns.load_dataset("iris")
 
-
-dataframe = sns.load_dataset("iris")
-
-#dataframe = pandas.read_csv("/home/pedro/Documents/git/ML/aulas_notbook/perception/iris.csv", header=None)
-dataset = dataframe.values
+dataset = df.values
 X = dataset[:,0:4].astype(float)
 Y = dataset[:,4]
 
-print(dataframe.head(4))
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+
+
+dummy_y = np_utils.to_categorical(encoded_Y)
+
+
+estimator = KerasClassifier(build_fn=baseline_model_MLP, epochs=200, batch_size=5, verbose=0)
+
+
+kfold = KFold(n_splits=10, shuffle=True)
+
+results = cross_val_score(estimator, X, dummy_y, cv=kfold)
+
+print("Baseline: %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
